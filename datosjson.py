58a2533320c061a6ecf707e92cd413f21bbfd02f
RodrigoDuocUC <rodr.guerrero@duocuc.cl>
2023-04-27 02:11:45 +0000

    Efectuando un commit del archivo datosjson.py en nuestro repositorio

---
+++ datosjson.py
@@ -0,0 +1,10 @@
+import json
+
+with open('myfile.json', 'r') as json_file:
+    ourjson = json.load(json_file)
+
+print("El token de acceso es: {}".format(ourjson['access_token']))
+print("El token expira en {} segundos.".format(ourjson['expires_in']))
+
+
+
